32b4ad1d91095a1f83501e7e3ef7a5f250ae4f11
John Doe <sk8vetal@gmail.com>
2022-03-25 19:48:46 +0200

    added on tap animation, refactoring code

--- lib/infrastructure/theme/app_colors.dart
+++ lib/infrastructure/theme/app_colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const darkBlue = Color(0xFF052640);
+  static const orange = Color(0x00ed4945);
 }
--- lib/movie/screens/movie_screen.dart
+++ lib/movie/screens/movie_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie/infrastructure/theme/app_colors.dart';
+import 'package:movie/movie/domain/movie.dart';
 import 'package:movie/movie/screens/cubit/movie_cubit.dart';
+
+// ---Texts---
+const _kTitle = 'Movie';
+
+// ---Parameters---
+const _kPadding = EdgeInsets.all(10);
+const _kGridDelegate = SliverGridDelegateWithMaxCrossAxisExtent(
+  maxCrossAxisExtent: 300,
+  mainAxisSpacing: 10,
+  crossAxisSpacing: 10,
+  childAspectRatio: 2 / 3,
+);
+const _kRadiusCircular = Radius.circular(15);
+const double _kHeight = 30;
+const double _kFontSize = 15;
+const double _kBlurRadius = 3;
+const double _kRadius = 15;
 
 class MovieScreen extends StatefulWidget {
   static const _routeName = '/movie-screen';
@@ -37,7 +55,7 @@ class _MovieScreenState extends State<MovieScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Movie'),
+        title: const Text(_kTitle),
       ),
       body: BlocBuilder<MovieCubit, MovieState>(
         builder: (context, state) {
@@ -47,53 +65,14 @@ class _MovieScreenState extends State<MovieScreen> {
             );
           } else if (state is MovieSuccess && state.movies != null) {
             return GridView.builder(
-              padding: const EdgeInsets.all(10),
+              padding: _kPadding,
               itemCount: state.movies!.length,
               itemBuilder: (BuildContext context, int index) {
                 final movie = state.movies![index];
 
-                return Container(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: AppColors.darkBlue.withOpacity(0.8),
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(15),
-                        bottomRight: Radius.circular(15),
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    height: 30,
-                    child: Text(
-                      movie.title,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 3,
-                        color: AppColors.darkBlue,
-                      ),
-                    ],
-                    borderRadius: BorderRadius.circular(15),
-                    image: DecorationImage(
-                      image: NetworkImage(movie.posterPath),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
+                return MovieCard(movie: movie);
               },
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 300,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                childAspectRatio: 2 / 3,
-              ),
+              gridDelegate: _kGridDelegate,
             );
           } else if (state is MovieError) {
             return const Center();
@@ -101,6 +80,85 @@ class _MovieScreenState extends State<MovieScreen> {
 
           return const SizedBox();
         },
+      ),
+    );
+  }
+}
+
+class MovieCard extends StatelessWidget {
+  const MovieCard({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ImageCard(movie: movie),
+        Positioned.fill(
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              focusColor: AppColors.darkBlue,
+              hoverColor: AppColors.darkBlue,
+              highlightColor: AppColors.darkBlue,
+              splashColor: AppColors.darkBlue,
+              borderRadius: BorderRadius.circular(_kRadius),
+              onTap: () {},
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ImageCard extends StatelessWidget {
+  const ImageCard({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.darkBlue.withOpacity(0.8),
+          borderRadius: const BorderRadius.only(
+            bottomLeft: _kRadiusCircular,
+            bottomRight: _kRadiusCircular,
+          ),
+        ),
+        alignment: Alignment.center,
+        height: _kHeight,
+        child: Text(
+          movie.title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: _kFontSize,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        boxShadow: const [
+          BoxShadow(
+            blurRadius: _kBlurRadius,
+            color: AppColors.orange,
+          ),
+        ],
+        borderRadius: BorderRadius.circular(_kRadius),
+        image: DecorationImage(
+          image: NetworkImage(movie.posterPath),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
